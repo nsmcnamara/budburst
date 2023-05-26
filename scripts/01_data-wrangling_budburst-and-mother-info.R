7f217c98c6bf939b40bdbbe54007aef49e91cbf3
Simone McNamara <mcnamara.simone@gmail.com>
2023-05-26 11:15:54 +0200

    imports and checks out budburst data

--- scripts/01_data-wrangling_budburst-and-mother-info.R
+++ scripts/01_data-wrangling_budburst-and-mother-info.R
@@ -1,4 +1,4 @@
-### Data import, cleaning and transforming for budburst scoring and information from mother trees.
+### Data import, cleaning and transformation for budburst scoring and information from mother trees.
 ### This script is part of the ACORN budburst analysis project
 ### Last update:  2023-05-26
 ### Simone McNamara
@@ -7,4 +7,26 @@
 #### Setup ####
 # libraries
 library(tidyverse)
+
+#### Data Import ####
+### Importing budburst scoring
+budburst <- read.csv("~/budburst/data/processed/20230526_budburst-zurich_corrected.csv", stringsAsFactors=TRUE)
+
+### Importing information from mother trees
+mother_info <- read.csv("~/budburst/data/processed/20230526_mother-info_corrected.csv", stringsAsFactors=TRUE)
+
+# Budburst
+## Checking out the data
+str(budburst)
+glimpse(budburst)
+
+## Checking NAs
+budburst %>%
+  summarise(across(everything(), ~ sum(is.na(.))))
+
+
+  
+  
+### Transforming the data
+### Budburst
 
