ecf713292f9198fe0565d89a91479478ddc4a61f
Simone McNamara <mcnamara.simone@gmail.com>
2023-05-26 08:53:06 +0200

    create file for data import, cleaning and transformation for budburst scoring and information from mother trees

---
+++ scripts/01_data-wrangling_budburst-and-mother-info.R
@@ -0,0 +1,10 @@
+### Data import, cleaning and transforming for budburst scoring and information from mother trees.
+### This script is part of the ACORN budburst analysis project
+### Last update:  2023-05-26
+### Simone McNamara
+
+
+#### Setup ####
+# libraries
+library(tidyverse)
+
