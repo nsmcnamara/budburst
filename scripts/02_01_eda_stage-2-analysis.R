cc26e7bead96f28e20d2bd4316b669923936caee
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-05-31 09:17:24 +0200

    add GGally EDA

--- scripts/02_01_eda_stage-2-analysis.R
+++ scripts/02_01_eda_stage-2-analysis.R
@@ -6,21 +6,35 @@
 #### Setup ####
 # libraries
 library(tidyverse)
-library(FactoMineR)
+library(GGally)
 
 #### Data Import ####
 stage_2_for_analysis <- read.csv("~/budburst/data/processed/stage_2_for_analysis.csv", stringsAsFactors=TRUE)
 
 glimpse(stage_2_for_analysis)
+head(stage_2_for_analysis)
+summary(stage_2_for_analysis)
+str(stage_2_for_analysis)
+
+### check NAs
+sapply(stage_2_for_analysis, function(x) sum(is.na(x)))
+
 
 #### First Data Viz ####
 
-hist(stage_2_for_analysis$doy_stage_2)
+### Histogram by species
+ggplot(stage_2_for_analysis,
+       mapping = aes(doy_stage_2, ..density.., fill = species, alpha = 0.5)) +
+  geom_histogram() +
+  geom_density() +
+  facet_wrap( ~species)
 
-# PCA
-# reorder columns
-stage_2_for_analysis <- stage_2_for_analysis %>%
-  mutate(age = as.numeric(age)) %>%
-  select(5:8, everything())
+### Automatic EDA by ggally
+stage_2_for_analysis %>%
+  select(doy_stage_2, species, altitude, latitude, longitude, age) %>%
+  ggpairs(mapping = aes(color = species, alpha = 0.5))
 
-pca <- PCA(stage_2_for_analysis, quali.sup = 1:7)
+
+## make a raincloudplot of doy by species
+## make a plot of doy by site_name
+
