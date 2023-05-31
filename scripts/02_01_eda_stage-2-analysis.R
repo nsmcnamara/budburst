4ad31fada04c9c79c1ed53038050afbca5cc3197
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-05-31 10:00:38 +0200

    add raincloudplot

--- scripts/02_01_eda_stage-2-analysis.R
+++ scripts/02_01_eda_stage-2-analysis.R
@@ -34,7 +34,54 @@ stage_2_for_analysis %>%
   select(doy_stage_2, species, altitude, latitude, longitude, age) %>%
   ggpairs(mapping = aes(color = species, alpha = 0.5))
 
-
 ## make a raincloudplot of doy by species
+ggplot(stage_2_for_analysis, aes(x = species, y = doy_stage_2, 
+                                 colour = species, fill = species, alpha = 0.5)) +
+  ggdist::stat_halfeye(
+    adjust = 0.5,
+    width = 0.6,
+    justification = -.2,
+    .width = 0,
+    point_colour = NA
+  ) +
+  geom_boxplot(
+    width = .12,
+    show.legend = FALSE
+  ) +
+  ggdist::geom_dotsinterval(
+    side = "left",
+    scale = 2
+  )+
+#  ggdist::stat_dots(
+#    side = "left",
+#    justification = 1.1,
+#    position = position_dodge(),
+#    binwidth = 7,
+#    dotsize = 0.5,
+#    scale = 2/10,
+#    show.legend = FALSE
+#  ) +
+#  geom_point(
+#    size = 0.3,
+#    position = position_jitterdodge(
+#      seed = 1,
+#      jitter.width = 0.3,
+#      jitter.height = 0.3,
+#      dodge.width = 0.5
+#    )
+#  ) +
+  coord_cartesian(xlim = c(1.2, NA)) +
+  scale_y_continuous(breaks = seq(100, 150, 10)) +
+  theme_bw() +
+  labs(x = "Species",
+       y = "DOY of Stage 2",
+       colour = "Species") +
+  scale_alpha(guide = "none") +
+  labs(fill = "Species", colour = "Species")
+
+
+
+
+
 ## make a plot of doy by site_name
 
