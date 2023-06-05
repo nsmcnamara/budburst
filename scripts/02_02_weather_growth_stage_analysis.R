128d1ae6e180f8cacffa721c5bd900c0b3b08489
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-05 08:42:43 +0200

    add idea

--- scripts/02_02_weather_growth_stage_analysis.R
+++ scripts/02_02_weather_growth_stage_analysis.R
@@ -7,3 +7,13 @@
 # libraries
 library(tidyverse)
 library(RColorBrewer)
+
+#### IDEA ####
+# plot mean, min and max temp jan till may (weather graph)
+# plot cumulative mean, min and max temp jan till may: gdd
+# include growth stage like here: https://www.arable.com/blog/a-guide-to-growing-degree-days-gdd-linking-temperature-and-crop-growth-stages/
+
+
+### Data import
+# weather
+# need avg date for stage 1/2/3/4/5 overall
