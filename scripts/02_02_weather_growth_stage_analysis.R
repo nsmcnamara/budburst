4a6e43d8c12f5bc9bad9bd80bf4257ab101b0cea
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-05 08:39:46 +0200

    make new file and include libraries

---
+++ scripts/02_02_weather_growth_stage_analysis.R
@@ -0,0 +1,9 @@
+### Exploratory Data Analysis for Weather/Growth Stage Analysis
+### This script is part of the ACORN budburst analysis project
+### Last update:  2023-06-05
+### Simone McNamara
+
+#### Setup ####
+# libraries
+library(tidyverse)
+library(RColorBrewer)
