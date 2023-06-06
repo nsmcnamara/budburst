2418cf00bc5cbf57eecbf1f61ca1d56f20701188
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-06 08:34:11 +0200

    make df w/o NA values, ie only 780 from 2023

--- scripts/02_03_eda_time-to-five-analysis.R
+++ scripts/02_03_eda_time-to-five-analysis.R
@@ -9,3 +9,21 @@
 library(tidyverse)
 library(GGally)
 library(RColorBrewer)
+
+
+#### Data Import ####
+gdd_2_to_5 <- read.csv("~/budburst/data/processed/gdd_2_to_5.csv", stringsAsFactors=TRUE)
+
+## Check out data
+glimpse(gdd_2_to_5)
+head(gdd_2_to_5)
+summary(gdd_2_to_5)
+str(gdd_2_to_5)
+
+## check NAs
+sapply(gdd_2_to_5, function(x) sum(is.na(x)))
+
+### DF w/o NA (no weather data for 2022, ergo no gdd)
+gdd_2_to_5_clean <- gdd_2_to_5 %>%
+  drop_na(gdd_2_to_5)
+  
