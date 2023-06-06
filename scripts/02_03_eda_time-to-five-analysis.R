7de2deb7c29d9e525420837fe4cc923b108e306b
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-06 08:28:50 +0200

    create file and set up libraries

---
+++ scripts/02_03_eda_time-to-five-analysis.R
@@ -0,0 +1,11 @@
+### Exploratory Data Analysis for Time to 5 Analysis
+### This script is part of the ACORN budburst analysis project
+### Last update:  2023-06-06
+### Simone McNamara
+
+
+#### Setup ####
+# libraries
+library(tidyverse)
+library(GGally)
+library(RColorBrewer)
