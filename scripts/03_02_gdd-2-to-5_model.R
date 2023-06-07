542f0346cae1122edb2a027aeb91ef8dea27b878
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-07 12:26:16 +0200

    creates file and setup

---
+++ scripts/03_02_gdd-2-to-5_model.R
@@ -0,0 +1,12 @@
+### Model for Time to 5 Analysis
+### This script is part of the ACORN budburst analysis project
+### Last update:  2023-06-07
+### Simone McNamara
+
+
+#### Setup ####
+# libraries
+library(tidyverse)
+
+# Palette
+my_pal <- c("#B14343", "#936E23","#6E8938","#0C420C","#24785C","#1D3643", "#436379", "#4E376C", "#B9679E","#ED6F6F")
