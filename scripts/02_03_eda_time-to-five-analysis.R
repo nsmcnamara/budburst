576e1dc5aaedcdea0c98d6ef28b1050c6e5fe1fb
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-07 16:50:01 +0200

    plays with labels and means on gdd x latitude

--- scripts/02_03_eda_time-to-five-analysis.R
+++ scripts/02_03_eda_time-to-five-analysis.R
@@ -16,37 +16,37 @@ Dark2palette <- brewer.pal(8, "Dark2")
 my_pal <- c("#B14343", "#936E23","#6E8938","#0C420C","#24785C","#1D3643", "#436379", "#4E376C", "#B9679E","#ED6F6F")
 
 #### Data Import ####
-gdd_2_to_5 <- read.csv("~/budburst/data/processed/gdd_2_to_5_for_analysis.csv", stringsAsFactors=TRUE)
+df_gdd_2_to_5 <- read.csv("~/budburst/data/processed/gdd_2_to_5_for_analysis.csv", stringsAsFactors=TRUE)
 
 ## Check out data
-glimpse(gdd_2_to_5)
-head(gdd_2_to_5)
-summary(gdd_2_to_5)
-str(gdd_2_to_5)
+glimpse(df_gdd_2_to_5)
+head(df_gdd_2_to_5)
+summary(df_gdd_2_to_5)
+str(df_gdd_2_to_5)
 
 ## check NAs
-sapply(gdd_2_to_5, function(x) sum(is.na(x)))
+sapply(df_gdd_2_to_5, function(x) sum(is.na(x)))
 
 ### DF w/o NA (no weather data for 2022, ergo no gdd)
-gdd_2_to_5_clean <- gdd_2_to_5 %>%
+df_gdd_2_to_5_clean <- df_gdd_2_to_5 %>%
   drop_na(gdd_2_to_5)
 
 # calculate means and counts  
-means <- gdd_2_to_5_clean %>%
+means <- df_gdd_2_to_5_clean %>%
   group_by(species) %>%
   summarize(m = mean(gdd_2_to_5))
 ### very close, no diff by species
 
-counts <- gdd_2_to_5_clean %>%
+counts <- df_gdd_2_to_5_clean %>%
   group_by(species) %>%
   summarise(n = n())
 
-sum_stats <- gdd_2_to_5_clean %>%
+sum_stats <- df_gdd_2_to_5_clean %>%
   group_by(species, site_name, age, latitude, longitude, altitude) %>%
   summarize(mean = mean(gdd_2_to_5))
 
 #### GDD 2 to 5 Q. petraea ####
-gdd_2_to_5_clean %>%
+df_gdd_2_to_5_clean %>%
   filter(species == "Q.petraea") %>%
   filter(age == "2") %>%
   ggplot(aes(x = reorder(site_name, altitude), 
@@ -97,3 +97,88 @@ gdd_2_to_5_clean %>%
 
 
 #### Q. robur only ####
+df_robur_only <- df_gdd_2_to_5_clean %>%
+  filter(species == "Q.robur")
+
+robur_means <- aggregate(gdd_2_to_5 ~ site_name + altitude + longitude + latitude + site_wet, df_robur_only, mean)
+
+## ALTITUDE
+ggplot(data = df_robur_only, 
+       mapping = aes(x = altitude, y = gdd_2_to_5)) +
+  geom_point() +
+  stat_summary(
+    fun = mean,
+    geom = "text",
+    aes(x = altitude, y = gdd_2_to_5, label = round(..y.., 2)),
+    size = 3,
+    color = "red",
+    show.legend = FALSE,
+    vjust = -1
+  ) +
+  geom_smooth(
+    method = "lm",
+    se = FALSE,
+    color = "blue"
+  )
+
+## LATITUDE
+df_site_name_unique_robur <- distinct(df_robur_only, site_name, .keep_all = TRUE) %>%
+  filter(species == "Q.robur")
+
+ggplot(data = df_robur_only, 
+       mapping = aes(x = latitude, y = gdd_2_to_5, colour = site_name)) +
+  geom_point(size = 5, alpha = 0.5, position = position_dodge(width = 0.000000000000000000000000000005)) +
+  stat_summary(
+    fun = mean,
+    geom = "text",
+    aes(x = latitude, y = gdd_2_to_5, label = round(..y.., 2)),
+    size = 5,
+    color = "red",
+    show.legend = FALSE,
+    vjust = -9
+  ) +
+  geom_smooth(
+    method = "lm",
+    se = FALSE,
+    color = "blue"
+  ) +
+  scale_color_manual(values = my_pal) +
+  geom_label_repel(data = df_site_name_unique_robur, aes(label = site_name))
+
+## LONGITUDE
+ggplot(data = df_robur_only, 
+       mapping = aes(x = longitude, y = gdd_2_to_5, colour = site_name)) +
+  geom_point() +
+  stat_summary(
+    fun = mean,
+    geom = "text",
+    aes(x = longitude, y = gdd_2_to_5, label = round(..y.., 2)),
+    size = 3,
+    color = "red",
+    show.legend = FALSE,
+    vjust = -1
+  ) +
+  geom_smooth(
+    method = "lm",
+    se = FALSE,
+    color = "blue"
+  ) +
+  scale_color_manual(values = my_pal)
+
+ggplot(data = df_robur_only, 
+       mapping = aes(x = site_wet, y = gdd_2_to_5)) +
+  geom_point() +
+  stat_summary(
+    fun = mean,
+    geom = "text",
+    aes(x = latitude, y = gdd_2_to_5, label = round(..y.., 2)),
+    size = 3,
+    color = "red",
+    show.legend = FALSE,
+    vjust = -1
+  ) +
+  geom_smooth(
+    method = "lm",
+    se = FALSE,
+    color = "blue"
+  )
