2e84f7039a9067ef5362cf2833556bd024396f07
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-16 13:02:11 +0200

    make fuller model and check model assumptions

--- scripts/03_01_gdd-stage-2_model.R
+++ scripts/03_01_gdd-stage-2_model.R
@@ -30,9 +30,52 @@ chelsa <- chelsa %>%
 
 # rename
 colnames(chelsa) <- gsub("CHELSA_|_1981.2010_V.2.1", "", colnames(chelsa))
+chelsa <- chelsa %>%
+  rename(temp_ann_mean = bio1) %>%
+  rename(precip_ann = bio12) %>%
+  rename(precip_seasonality = bio15)
 
 
 df_s2 <- left_join(stage_2_for_analysis, chelsa, by = "mother_id")
+
+
+### plot variables and transform if necessary
+# as factor
+df_s2 <- df_s2 %>%
+  mutate(age = as.factor(age)) %>%
+  mutate(year = as.factor(year))
+
+# plot all numeric variables
+numeric_columns <- df_s2[ , sapply(df_s2, is.numeric)]
+par(mfrow = c(4, 6))  # Set up multiple plots in one row
+lapply(colnames(numeric_columns), function(col_name) {
+  hist(numeric_columns[[col_name]], main = col_name, xlab = "Value", ylab = "Frequency")
+})
+
+# transformations
+hist(sqrt(df_s2$altitude)) # alt: sqrt (right-skewed)
+hist(df_s2$latitude) # i think keep
+hist(df_s2$longitude) # i think keep
+hist(log(df_s2$gdd_above_5)) # gdd_above_5: log 
+e1071::skewness(df_s2$temp_ann_mean)
+hist(log(df_s2$temp_ann_mean)) # temp_ann_mean: log
+hist(log(df_s2$precip_ann)) # precip: log
+hist(df_s2$fcf) # i think keep
+hist(log(df_s2$gdd5)) # gdd5: log
+hist(df_s2$gsl) # keep
+hist(df_s2$gsp) # keep
+
+df_s2 <- df_s2 %>%
+  mutate(alt_sqrt = sqrt(altitude)) %>%
+  mutate(gdd_above_5_log = log(gdd_above_5)) %>%
+  mutate(temp_ann_mean_log = log(temp_ann_mean)) %>%
+  mutate(precip_ann_log = log(precip_ann)) %>%
+  mutate(gdd5_log = log(gdd5))
+    
+
+
+
+
 
 #### LMM Stage 2 ####
 # resp ~ FEexpr + (REexpr1 | factor1) + (REexpr2 | factor2) + ...
@@ -144,16 +187,40 @@ m_gdd_s2_rob_aust_alt_wet <- df_s2 %>%
 summary(m_gdd_s2_rob_aust_alt_wet)
 
 
+### LMM robur with many fun things
+m_gdd_rob_many <- df_s2 %>%
+  filter(species == "Q.robur") %>%
+  filter(site_name != "Bosco_Pantano") %>%
+  lmer(gdd_above_5 ~ latitude + altitude + temp_ann_mean + precip_ann + gsl + 
+         (1 | mother_id) + (1 | age) + (1 | year), data = .)
 
+
+summary(m_gdd_rob_many)
+
+
+### check model assumptions
+
+## Tukey Anscombe
+par(mfrow = c(1,1))
+plot(m_gdd_rob_many)
+
+## QQ plot
+qqnorm(resid(m_gdd_rob_many))
+qqline(resid(m_gdd_rob_many))
+
+## scale-location plot
+plot(m_gdd_rob_many,
+     sqrt(abs(resid(.)))~fitted(.),
+     type=c("p","smooth"), col.line=1)
+
+## leverage plot
+plot(m_gdd_rob_many, rstudent(.) ~ hatvalues(.))
+performance::check_model(m_gdd_rob_many)
 
 
 
 ### ### ### ### ### ### ###
-
-### check model assumptions
-### Tukey Anscombe
-plot(model_growing_degree_days) # welll.... 
-
+### ### ### ### ### ### ###
 # QQ of residuals
 qqnorm(resid(model_growing_degree_days), main = "Residuals")
 qqline(resid(model_growing_degree_days), col = "red") # looks ok to me
