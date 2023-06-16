696e18d984bfb85168230a5b55f79fb74b0109ce
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-16 10:43:53 +0200

    add more chelsa files

--- scripts/01_02_chelsa.R
+++ scripts/01_02_chelsa.R
@@ -4,28 +4,40 @@ library(sf)
 
 library(raster)
 
-coordinates.testing <- read.delim("/Users/simonemcnamara/Desktop/UZH/MSc Thesis/30 Data Analysis/mother.coords.no.spp.txt") ## your coordinate file (ID Long Lat )
 
-
-coordinates.testing <- read.csv("/Users/simonemcnamara/budburst/data/processed/mother-info.csv") ## your coordinate file (ID Long Lat )
+coordinates.testing <- read.csv("~/budburst/data/processed/mother-info.csv") ## your coordinate file (ID Long Lat )
 coordinates <- coordinates.testing %>%
   dplyr::select(mother_id, longitude, latitude) %>%
   rename(Long = longitude) %>%
   rename(Lat = latitude)
 
-rasStack = stack("/Users/simonemcnamara/CHELSA_kg2_1981-2010_V.2.1.tif ") ## whatever file to add more files you'd seperate the file names with a comma
+## whatever file to add more files you'd seperate the file names with a comma
+rasStack = stack("~/budburst/data/raw/CHELSA_bio1_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_bio12_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_bio15_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_fcf_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gdd5_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gdd10_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gddlgd10_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gdgfgd5_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gdgfgd10_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gsl_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gsp_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_gst_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_kg2_1981-2010_V.2.1.tif",
+                 "~/budburst/data/raw/CHELSA_ngd5_1981-2010_V.2.1.tif"
+                ) 
+
 
 pointCoordinates=coordinates
 
 coordinates(pointCoordinates)= ~ Long + Lat ## match with column headers
 
 rasValue=extract(rasStack, pointCoordinates)
-#years <- 2010-1980
-#rasValue_pa = rasValue/years # calculate gdd per year
 
 combinePointValue=cbind(pointCoordinates,rasValue)
 
-write.table(combinePointValue,file="~/kg2.csv", append=FALSE, sep= ",", row.names = FALSE, col.names=TRUE) ## table
+write.table(combinePointValue,file="~/budburst/data/processed/coordinates_chelsa_values.csv", append=FALSE, sep= ",", row.names = FALSE, col.names=TRUE) ## table
 
 
 ### debbie's original code
