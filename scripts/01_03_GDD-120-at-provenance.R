6a1c94249ad97736925908595f01641109f485ac
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-22 13:55:29 +0200

    for loop for one year

--- scripts/01_03_GDD-120-at-provenance.R
+++ scripts/01_03_GDD-120-at-provenance.R
@@ -56,6 +56,51 @@ temp_month_base <- ifelse(temp_month_celsius < tbase, 0, temp_month_celsius)
 # cumulate for month
 temp_month_base_cum <- t(apply(temp_month_base, 1, cumsum))
 
+
+# do it for one year
+
+# Define the base directory and file pattern
+base_dir <- "data/ISIMIP3a/CHELSA-W5E5v1.0/"
+file_pattern <- "chelsa-w5e5v1.0_obsclim_tas_1800arcsec_global_daily_2016%02d.nc"
+
+# Define the start and end indices based on file endings
+start_index <- 1  # Assuming the first file ends with "01"
+end_index <- 1    # Assuming the last file ends with "04"
+
+# Create an empty data frame
+cumulative_temps_df <- data.frame()
+
+# Loop through the files
+for (i in start_index:end_index) {
+  # Construct the file path
+  file_path <- list.files(path = base_dir, pattern = sprintf(file_pattern, i), full.names = TRUE)
+  
+  # Read the temperature data from the file
+  stack <- stack(file_path)
+  
+  # Extract values for coordinates
+  temp_month <- extract(stack, unique_coordinates)
+  
+  # Convert to °C
+  temp_month_celsius <- temp_month - kelvin
+  
+  # Set to 0 if below 5, otherwise subtract 5 to get gdd_5
+  temp_month_base <- ifelse(temp_month_celsius < tbase, 0, temp_month_celsius - tbase)
+  
+  # Calculate the cumulative sum for each month
+  temp_month_base_cum <- t(apply(temp_month_base, 1, cumsum))
+  
+  print(temp_month_base_cum[length(temp_month_base_cum)])
+  # Add the cumulative sum values as a new column in the data frame
+  # cumulative_temps_df <- cbind(cumulative_temps_df, temp_month_base_cum)
+}
+
+# Rename the columns of the data frame
+colnames(cumulative_temps_df) <- c("Month 1", "Month 2", "Month 3", "Month 4")
+
+
+
+
 # create a df that has an entry with growing degree days from january until april for each year in the dataset (1979 - 2016)
 # where growing degree days is the cumulative temperature above 5°C, and temperatures below that are discarded and not considered when calculating the growing degree days
 
