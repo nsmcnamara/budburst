47c1100f5f77abd19579fc7b932d62120d3dc798
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-22 13:14:51 +0200

    anova, var.test

--- scripts/01_04_chlorophyll-sulphur-calib.R
+++ scripts/01_04_chlorophyll-sulphur-calib.R
@@ -7,3 +7,24 @@
 #### Setup ####
 # libraries
 library(tidyverse)
+
+#### Data Import ####
+df_chlor_calib <- read.csv("~/budburst/data/processed/chlorophyll_sulphur_calib.csv", stringsAsFactors=TRUE)
+df_clean <- df_chlor_calib %>%
+  filter(status == "clean") %>%
+  drop_na()
+df_dirty <- df_chlor_calib %>%
+  filter(status == "dirty") %>%
+  drop_na()
+
+one.way <- aov(Reading ~ status, data = df_chlor_calib)
+summary(one.way)
+
+var.test(df_clean$Reading, df_dirty$Reading, alternative = "two.sided")
+
+mean(df_clean$Reading)
+mean(df_chlor_calib$Reading[df_chlor_calib$status == "clean"], na.rm = TRUE)
+var(df_clean$Reading)
+
+mean(df_dirty$Reading)
+var(df_dirty$Reading)
