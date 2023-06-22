10011bb64d13fd886044eb6335fc1aa5e994f78b
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-06-22 11:19:42 +0200

    initial and setup

---
+++ scripts/01_04_chlorophyll-sulphur-calib.R
@@ -0,0 +1,9 @@
+### Checking whether sulphur spots affect chlorophyll measurements
+### This script is part of the ACORN budburst analysis project
+### Last update:  2023-06-12
+### Simone McNamara
+
+
+#### Setup ####
+# libraries
+library(tidyverse)
