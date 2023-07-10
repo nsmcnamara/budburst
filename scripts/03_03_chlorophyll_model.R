9a8818bae4e1d28680d2c70b819b418ed81e1c1b
nsmcnamara <118906473+nsmcnamara@users.noreply.github.com>
2023-07-10 08:11:09 +0200

    Setup

--- scripts/03_03_chlorophyll_model.R
+++ scripts/03_03_chlorophyll_model.R
@@ -2,3 +2,11 @@
 ### This script is part of the ACORN budburst analysis project
 ### Last update:  2023-07-10
 ### Simone McNamara
+
+#### Setup ####
+# libraries
+library(tidyverse)
+
+
+# resolve conflicts
+library(conflicted)
